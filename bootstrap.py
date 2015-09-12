bf5c337587d1d20d8fee03ff420bb0a4b670c5ed
Qingping Hou <qihou@linkedin.com>
2015-09-11 17:50:49 -0700

    minor adjustments

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import sys
 
 
@@ -10,12 +11,19 @@ def activate_pex():
 
     sys.stderr.write('PEX bootstrap: entry_point=%s\n' % entry_point)
 
-    sys.path[0] = os.path.abspath(sys.path[0])
+    cur_path = os.path.abspath(sys.path.pop(0))
     sys.path.insert(0, entry_point)
     sys.path.insert(0, os.path.abspath(os.path.join(entry_point, '.bootstrap')))
+    old_sys_path = copy.copy(sys.path)
 
     from _pex.pex_bootstrapper import bootstrap_pex_env
     bootstrap_pex_env(entry_point)
+
+    # move system path to the end
+    sys.path = [p for p in sys.path if p not in old_sys_path]
+    sys.path += old_sys_path
+    # insert current working path so it's eaiser to hot patch app
+    sys.path.insert(0, cur_path)
     sys.stderr.write('PEX bootstrap: sys.path=%s\n\n' % sys.path)
 
 activate_pex()
